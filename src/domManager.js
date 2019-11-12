5379610bfa1bb9f7abdfe929c4f86bbe77d0725a
samuel <samuelgarciacompanys@gmail.com>
2019-11-12 20:05:04 +0100

    added styling changes

--- src/domManager.js
+++ src/domManager.js
@@ -3,71 +3,78 @@
 
 const domManager = (() => {
 
-    const temperature = document.querySelector('.temperature')
-    const humidity = document.querySelector('.humidity')
-    const weathertitle = document.querySelector('#weathertitle')
-    const key = 'c3e71b244675fb70f2b3fcf42479dbd8';
+  const temperature = document.querySelector('.temperature')
+  const humidity = document.querySelector('.humidity')
+  const weathertitle = document.querySelector('#weathertitle')
+  const key = 'c3e71b244675fb70f2b3fcf42479dbd8';
 
-    const toCelcius = (kelvin) => {
-      return Math.round(kelvin - 273.15)
-    }
+  const toCelcius = (kelvin) => {
+    return Math.round(kelvin - 273.15)
+  }
 
-    let weather = {
-        temperature: null,
-        humidity: null
-    }
-    
+  const toFar = (kelvin) => {
+    return Math.round(32+(toCelcius(kelvin)*9)/5)
+  }
 
-    var person = {
-        firstName: "John",
-        lastName: "Doe",
-        age: 50,
-        eyeColor: "blue"
-      };
-  
-    const renderData = (cityname) =>{
+  let weather = {
+    temperature: null,
+    humidity: null
+  }
 
-              let api = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${key}`;
-  
-              fetch(api)
-  
-              .then(function (response) {
-                let data = response.json()
-                console.log(data)
-                return data;
-              })
+  const renderData = (cityname) => {
 
-              .then(function (data) {
-                weather.temperature = data.main.temp
-                weather.humidity = data.main.humidity
+    let api = `http://api.openweathermap.org/data/2.5/weather?q=${cityname}&appid=${key}`;
 
-                temperature.innerHTML = `Temperature: ${toCelcius(weather.temperature)}`;
-                humidity.innerHTML = `Humidity: ${weather.humidity}`;
+    fetch(api)
 
-              })  
+      .then(function (response) {
+        let data = response.json()
+        console.log(data)
+        return data;
+      })
 
-                        
-            }
+      .then(function (data) {
+        weather.temperature = data.main.temp
+        weather.humidity = data.main.humidity
+        let celcius = true
 
-    const AddTitle = (cityname) =>{
-             weathertitle.textContent = `Weather in ${cityname}`
-      }
+        temperature.innerHTML = `Temperature: ${toCelcius(weather.temperature)}C/ F`;
+
+        temperature.addEventListener('click', (evt) => {
+
+          if(celcius){
+            temperature.innerHTML = `Temperature: ${toFar(weather.temperature)}F/ C`;
+            celcius = false;
+          }else{
+            temperature.innerHTML = `Temperature: ${toCelcius(weather.temperature)}C/ F`;
+            celcius = true;
+          }  
+        });
+
+        humidity.innerHTML = `Humidity: ${weather.humidity}%`;
+      })
+
+
+  }
+
+  const AddTitle = (cityname) => {
+    weathertitle.textContent = `Weather in ${cityname}`
+  }
 
 
 
-    const StartApp = () => {
+  const StartApp = () => {
 
-        document.getElementById(`weather-check`).addEventListener('click', (evt) => {
+    document.getElementById(`weather-check`).addEventListener('click', (evt) => {
 
-            let cityname = document.querySelector('#weather-check-input').value
+      let cityname = document.querySelector('#weather-check-input').value
+      AddTitle(cityname);
+      renderData(cityname);
 
-            AddTitle(cityname);
-            renderData(cityname);
-            
-          });
-    }
+    });
+  }
 
-    return{StartApp}
+  return { StartApp }
 })();
 
 export default domManager;
